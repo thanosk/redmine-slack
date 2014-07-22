e061deb7bf56209a423a633b6d8e7b73b3ff10b2
Takayuki SHIMIZUKAWA <shimizukawa@gmail.com>
2014-07-22 16:18:21 +0900

    fix: Field vals had never been displayed on Slack

--- lib/redmine_slack/listener.rb
+++ lib/redmine_slack/listener.rb
@@ -200,6 +200,8 @@ private
 			value.split("\n")[0,5].join("\n") + '...'
 		elsif value.length > 100
 			value[0,97] + '...'
+		else
+			value
 		end
 
 		result = { :title => title, :value => value }
